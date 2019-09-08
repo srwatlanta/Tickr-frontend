9b286482cfe12bcf1c3a381316cdfdd6c11966b6
christianalim <christianalim1@gmail.com>
2019-09-08 15:33:59 -0500

    framework for card

--- src/containers/StockShowContainer.js
+++ src/containers/StockShowContainer.js
@@ -4,11 +4,27 @@ import StockNews from '../components/StockNews'
 import StockInfo from '../components/StockInfo'
 import GraphChart from '../components/GraphChart'
 
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+
+
+  
 class StockShowContainer extends Component {
+    
+
     render() {
         return (
             <div>
-                
+                <Paper>
+                <Typography variant="h5" component="h3">
+                This is a sheet of paper.
+                </Typography>
+                <Typography component="p">
+                Paper can be used to build surface or other elements for your application.
+                </Typography>
+                </Paper>
             </div>
         );
     }
