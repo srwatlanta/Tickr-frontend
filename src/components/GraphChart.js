7a8b581743f467a02a8f5c9c28c291c5595a7c68
christianalim <christianalim1@gmail.com>
2019-09-08 16:02:41 -0500

    messing around with graph

--- src/components/GraphChart.js
+++ src/components/GraphChart.js
@@ -1,13 +1,76 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class GraphChart extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import {
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+  } from 'recharts';
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2),
+  },
+}));
+
+const data = [
+    {"name": "9/1",
+     "pv": 210
+    },
+    {"name": "9/2",
+     "pv": 215
+    },
+    {"name": "9/3",
+     "pv": 220
+    },
+    {"name": "9/4",
+     "pv": 190
+    },
+    {"name": "9/5",
+     "pv": 160
+    },
+    {"name": "9/6",
+     "pv": 140
+    },
+    {"name": "9/7",
+     "pv": 175
+    },
+    {"name": "9/8",
+     "pv": 190
+    },
+    {"name": "9/9",
+     "pv": 210
+    },
+    {"name": "9/10",
+    "pv": 185
     }
+]
+
+
+const GraphChart = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Paper className={classes.root}>
+        <Typography variant="h5" component="h3">
+          AAPL
+        </Typography>
+        <Typography component="p">
+          Sector: Technology
+        </Typography>
+        <LineChart width={730} height={250} data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </Paper>
+    </div>
+  );
 }
 
 export default GraphChart;
--- src/containers/StockShowContainer.js
+++ src/containers/StockShowContainer.js
@@ -17,14 +17,7 @@ class StockShowContainer extends Component {
     render() {
         return (
             <div>
-                <Paper>
-                <Typography variant="h5" component="h3">
-                This is a sheet of paper.
-                </Typography>
-                <Typography component="p">
-                Paper can be used to build surface or other elements for your application.
-                </Typography>
-                </Paper>
+                <GraphChart />
             </div>
         );
     }
